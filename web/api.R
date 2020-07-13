cd8950940b15415f6a746919feb6bb223f283a04
cookiecat <cookiecat@CookieCat.local>
2020-07-13 14:45:07 -0700

    endpoints for absolute.risk check.summary and risk.summary with example data

--- web/api.R
+++ web/api.R
@@ -1,22 +1,26 @@
-#library(BCRA)
+library(BCRA)
 
-#* Absolute Risk
-#* @post /absolute.risk
+#' @get /ohai
+function(req){
+    list("ohai - you made it")
+}
 
-#* @get /absolute.risk
+#' Absolute Risk
+#' @post /absolute.risk
+#' @param id
+#' @param age
+#' @param biopsies
+#' @param hyperplasia
+#' @param menstruation
+#' @param first_birth
+#' @param relatives
+#' @param race
+#' @param Raw_Ind
+#' @param Avg_white
+#' @get /absolute.risk
+#' all params need to be there or bad result
 function(req, age=35, biopsies=1, hyperplasia=1, menstruation=1, first_birth=1, relatives=1, race=1, Raw_Ind=1, Avg_White=1) {
-    # BrCa_1_AR.rda
-    # BrCa_beta.rda
-    # BrCa_lambda1.rda
-    # BrCa_lambda2.rda
-    # exampledata.rda
     cat(" age::", age, biopsies, hyperplasia, menstruation, first_birth, relatives, race, Raw_Ind, Avg_White)
-    source("../R/recode.check.R")
-    source("../R/relative.risk.R")
-    source("../R/absolute.risk.R")
-    #attach(file.path("..","data",data))
-    # Not sure how to switch the data input based on the 
-    # data variable passed in
     absolute.risk(data.frame(ID=1,
                              T1=as.numeric(age),
                              T2=as.numeric(age)+5,
@@ -31,12 +35,12 @@ function(req, age=35, biopsies=1, hyperplasia=1, menstruation=1, first_birth=1, 
                   )
 }
 
-#* Relative Risk 
+#* Relative Risk
 #* @param data_name The template data name
-#* @param Raw_Ind 
+#* @param Raw_Ind
 #* @get /relative.risk
 function(data="exampledata.rda", Raw_Ind=1) {
-    source("../R/recode.check.R")        
+    source("../R/recode.check.R")
     source("../R/relative.risk.R")
     attach(file.path("..","data",data))
     relative.risk(exampledata, Raw_Ind)
@@ -44,29 +48,21 @@ function(data="exampledata.rda", Raw_Ind=1) {
 
 #* Check Summary
 #* @param data_name The template data name
-#* @param Raw_Ind 
+#* @param Raw_Ind
 #* @param Avg_white
 #* @get /check.summary
-function(data="exampledata.rda", Raw_Ind=1, Avg_White=0) {    
-    source("../R/recode.check.R")
-    source("../R/relative.risk.R")
-    source("../R/absolute.risk.R")
-    source("../R/check.summary.R")
+function(data="exampledata.rda", Raw_Ind=1, Avg_White=0) {
     attach(file.path("..","data",data))
-    check.summary(exampledata, Raw_Ind, Avg_White)
+    check.summary(data.frame(exampledata), Raw_Ind, Avg_White)
 }
 
 #* Risk Summary
 #* @param data_name The template data name
-#* @param Raw_Ind 
+#* @param Raw_Ind
 #* @get /risk.summary
-function(data="exampledata.rda", Raw_Ind=1) {    
-    source("../R/recode.check.R")
-    source("../R/relative.risk.R")
-    source("../R/absolute.risk.R")
-    source("../R/risk.summary.R")
+function(data="exampledata.rda", Raw_Ind=1) {
     attach(file.path("..","data",data))
-    risk.summary(exampledata, Raw_Ind)
+    risk.summary(data.frame(exampledata), Raw_Ind)
 }
 
 
